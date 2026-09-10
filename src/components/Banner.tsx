134df17ae72ee8d3afa44499c5a1b9447ac47761
md-ashraful-islam-ullas <ashraful.ullas@gmail.com>
2026-09-10 22:24:22 +0600

    feat: add the banner section

---
+++ src/components/Banner.tsx
@@ -0,0 +1,26 @@
+import bannerStack from "../assets/banner-stack.png";
+
+const Banner = () => {
+  return (
+    <div className="mt-30 container mx-auto flex justify-between">
+      <div className="mt-30">
+        <p className="text-7xl font-bold">
+          Build Your Ideal <br />
+          <span className="bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">Development Stack</span>
+        </p>
+        <p className="text-2xl font-light leading-9 text-[#475569] mt-10 mb-20">
+          Explore frontend, backend, database, and tooling options,<br /> compare them
+          side by side, and put together the stack that fits your <br /> next project.
+        </p>
+        <div>
+            <button className="px-4 py-3 bg-amber-400 text-white rounded-xl bg-linear-to-r from-[#F97316] to-[#EC4899] mr-5 cursor-pointer">Explore Technologies</button>
+            <button className="px-11 py-3 text-[#374151] border-2 border-gray-300 rounded-xl cursor-pointer">Learn More</button>
+        </div>
+      </div>
+
+      <img className="mt-10 h-150" src={bannerStack} alt="" />
+    </div>
+  );
+};
+
+export default Banner;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import navLogo from "../assets/logo-text.png"
 
 const Nav = () => {
     return (
-        <nav className="fixed top-0 left-0 right-0 z-50 border-b border-gray-300">
+        <nav className="fixed top-0 left-0 right-0 z-50 border-b border-gray-300 bg-white">
             <div className="container mx-auto flex justify-between items-center h-22">
                 <img className="h-10" src={navLogo} alt="" />
 
@@ -15,8 +15,8 @@ const Nav = () => {
                 </ul>
 
                 <div className="flex gap-5 items-center">
-                    <p>Sign In</p>
-                    <button className="bg-[#D91B7E] text-white px-5 py-3 rounded-3xl">
+                    <p className="cursor-pointer">Sign In</p>
+                    <button className="bg-[#D91B7E] text-white px-5 py-3 rounded-3xl cursor-pointer">
                         Sign Up
                     </button>
                 </div>
